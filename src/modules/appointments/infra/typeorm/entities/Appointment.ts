b31449ace805e859a9e2c3d9336c40cacd4e47db
marquisalexander <marquissantos123@gmail.com>
2020-11-05 10:29:21 -0300

    alterando a arquitetura

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -1,4 +1,4 @@
-import { 
+import {
     Entity,
     Column,
     PrimaryGeneratedColumn,
@@ -8,7 +8,7 @@ import {
     JoinColumn
 } from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 
 @Entity('appointments')
--- src/modules/appointments/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/repositories/AppointmentsRepository.ts
@@ -1,17 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
     public async findByDate(date: Date): Promise<Appointment | null> {
         // percorrendo o arrau de appointments com o .find e verificando se a hora do parsedDate
         // já existe no array ou não 
-    const findAppointment = await this.findOne({
-        where: {date },
-    });
+        const findAppointment = await this.findOne({
+            where: { date },
+        });
 
-    return findAppointment || null;
+        return findAppointment || null;
     }
 }
 
